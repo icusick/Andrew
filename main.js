cccf2751ef6a703598ff78a84f671ca75a19ca58
isabel cusick <cusick.isabel@gmail.com>
2017-02-01 19:21:35 -0600

    fucks shit up like a pro

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('linked');
+
+$(document).ready(function() {
+
+
+// switch capability from http://stackoverflow.com/questions/19054111/replace-one-div-with-another-on-hover
+// $('.switch').hover(function() {
+// 		$(this).find('.header').hide();
+// 		$(this).find('.nav').show();
+// 	}, function() {
+// 		$(this).find('.nav').hide();
+// 		$(this).find('.header').show();
+// }); 
+
+
+
+
+
+
+
+
+
+
+
+});
